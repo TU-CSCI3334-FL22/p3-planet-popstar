1cce1d6a1a65059c0ac85f07fae914534da679d7
Youngone Lee <ylee5@trinity.edu>
2022-10-20 16:43:36 -0500

    created some function skeletons for the parsing methods

--- Reader.hs
+++ Reader.hs
@@ -3,9 +3,9 @@ module Reader where
 type Symbol = String
 type Terminal = String
 type NonTerminal = String
--- type SymbolTable = [(String, Int)] --Map of strings  to ints 
-data Token = SEMICOLON | ALSODERIVES | EPSILON | DERIVES | SYMBOL deriving Show
-data IR = Undefined2
+data Token = SEMICOLON | ALSODERIVES | EPSILON | DERIVES | SYMBOL String deriving Show
+type Production = (NonTerminal, [Symbol])
+data IR = IR [Production] [Terminal] [Symbol]
 
 
 tokenize :: String -> Token
@@ -15,12 +15,37 @@ tokenize "epsilon" = EPSILON
 tokenize "Epsilon" = EPSILON
 tokenize "EPSILON" = EPSILON
 tokenize ":" = DERIVES
-tokenize _ = SYMBOL
+tokenize s = SYMBOL s
+
+getSymbols :: [Token] -> [Symbol] -- Unique only
+getSymbols = undefined
 
 grammarScan :: String -> ([Token], [Symbol])
-grammarScan fileString = (map tokenize (words fileString), words fileString)
+grammarScan fileString = let tokens = map tokenize (words fileString) in (tokens, getSymbols tokens)
 
+parseGrammar :: [Token] -> (IR, [Token])
+parseGrammar tokens = 
+    let (productionList, newTokens) = parseProductionList tokens
+    in undefined
+
+parseProductionList :: [Token] -> ([Production], [Token])
+parseProductionList = undefined
+
+parseSymbolList :: [Token] -> ([Symbol], [Token])
+parseSymbolList ((SYMBOL s):tokens) = 
+    let (newSymbols, newTokens) = parseSymbolListPrime tokens
+    in  (s:newSymbols, newTokens)
+
+parseSymbolListPrime :: [Token] -> ([Symbol], [Token])
+--line 14
+parseSymbolListPrime tokens@(ALSODERIVES:_) = ([], tokens)
+parseSymbolListPrime tokens@(SEMICOLON:_) = ([], tokens)
+--line 13
+parseSymbolListPrime ((SYMBOL s):tokens) = 
+    let (newSymbols, newTokens) = parseSymbolListPrime tokens
+    in  (s:newSymbols, newTokens)
 
 --IR, symbol table, list of non-terminals You can move the generation of the symbol table to here if you want.
-grammarParse :: ([Token], [Symbol]) -> (IR, [Terminal], [NonTerminal]) 
+grammarParse :: ([Token], [Symbol]) -> IR
+-- grammarParse _ = (parseIR _, parseTerminals _, parseNonTerminals _)
 grammarParse = undefined
