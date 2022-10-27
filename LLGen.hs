ddf074bd7ba57bc4c9dcae999e7a983dc75d82bd
Youngone Lee <ylee5@trinity.edu>
2022-10-27 12:25:32 -0500

    working on the first table

--- LLGen.hs
+++ LLGen.hs
@@ -1,20 +1,26 @@
 module LLGen where
 import Reader
 
-type FirstTable = [(Symbol, [NonTerminal])]
+type FirstTable = [(Symbol, [Terminal])]
 type FollowTable = [(NonTerminal, [Symbol])]
 type NextTable = [(Int, [Symbol])]
 
+makeTableFirstHelper :: (IR, [Token]) -> [Terminal] 
+makeTableFirstHelper = undefined 
 
 makeTableFirst :: (IR, [Token]) -> FirstTable
-makeTableFirst = undefined
+makeTableFirst ((IR productions, terminals, nonterminals), tokens) = 
+    let terminalList = [ (x, [x]) | x <- terminals ] -- purely terminals
+        finalNTList = [ (y, makeTableFirstHelper y) | y <- nonterminals ]
+    in  _ 
 
 makeTableFollow = undefined 
 makeTableNext = undefined 
 makeTableWorklist = undefined
 
-makeTables :: (IR, [Terminal], [NonTerminal]) -> Bool -> (FirstTable, FollowTable, NextTable)
-makeTables = undefined
+-- makeTables :: (IR, [Terminal], [NonTerminal]) -> Bool -> (FirstTable, FollowTable, NextTable)
+makeTables :: (IR, [Terminal], [NonTerminal]) -> FirstTable
+makeTables ((IR productions, terminals, nonterminals), givenTerminals, )= undefined
 
 --- ignore for now ^^ 
 
